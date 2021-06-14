a41f296effb679d4a964ffea3d70e9c76f7573b8
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-14 18:20:22 +0900

    Initial commit

---
+++ android/app/src/main/java/com/example/login_dice/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.login_dice;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+    );
+  }
+}
