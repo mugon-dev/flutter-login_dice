4a6770ed1102b2cda66f8a21a5f7996f7d43aca6
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-14 18:46:03 +0900

    add login page, form, button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,9 +18,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +37,67 @@ class Login extends StatelessWidget {
           onPressed: () {},
         ),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child: Image(
+                image: AssetImage("image/chef.gif"),
+                width: 170.0,
+                height: 190.0,
+              ),
+            ),
+            Form(
+              child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(40.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "dice"',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter Password',
+                        ),
+                        keyboardType: TextInputType.text,
+                        // text ** 처리
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 40.0),
+                      ButtonTheme(
+                        minWidth: 100.0,
+                        height: 50.0,
+                        child: FlatButton(
+                          color: Colors.orangeAccent,
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
