da17dfcf99801917d7f6bfa06082814d0a00fc2d
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-14 19:06:32 +0900

    add formfield controller, vaildate, snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'dice.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -26,6 +28,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,67 +43,123 @@ class _LoginState extends State<Login> {
         ),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage("image/chef.gif"),
-                width: 170.0,
-                height: 190.0,
-              ),
-            ),
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15.0,
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Center(
+                  child: Image(
+                    image: AssetImage("image/chef.gif"),
+                    width: 170.0,
+                    height: 190.0,
+                  ),
+                ),
+                Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                              labelText: 'Enter "dice"',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            controller: controller2,
+                            decoration: InputDecoration(
+                              labelText: 'Enter Password',
+                            ),
+                            keyboardType: TextInputType.text,
+                            // text ** 처리
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 40.0),
+                          ButtonTheme(
+                            minWidth: 100.0,
+                            height: 50.0,
+                            child: FlatButton(
+                              color: Colors.orangeAccent,
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                if (controller.text == 'dice' &&
+                                    controller2.text == '1234') {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              Dice()));
+                                } else if (controller.text == 'dice' &&
+                                    controller2.text != '1234') {
+                                  showSnackBar2(context);
+                                } else if (controller.text != 'dice' &&
+                                    controller2.text == '1234') {
+                                  showSnackBar3(context);
+                                } else {
+                                  showSnackBar(context);
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter "dice"',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter Password',
-                        ),
-                        keyboardType: TextInputType.text,
-                        // text ** 처리
-                        obscureText: true,
-                      ),
-                      SizedBox(height: 40.0),
-                      ButtonTheme(
-                        minWidth: 100.0,
-                        height: 50.0,
-                        child: FlatButton(
-                          color: Colors.orangeAccent,
-                          child: Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      "로그인 정보를 다시 확인하세요",
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      "비밀번호가 일치하지 않습니다.",
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      "dice의 철자를 확인하세요",
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
